fb1227b187dcecf81314cd983a9eaf90e12434d3
hashikawa <hashikawa.yuuichi@gmail.com>
2018-03-07 16:31:54 +0900

    Added problem 7

---
+++ 007/main-2.lisp
@@ -0,0 +1,40 @@
+#! /bin/env clisp
+
+(defun primes-to (n)
+  (let
+    ((sieved-array (sieve-to n))
+     (accum-list nil))
+    (do
+      ((i 2 (1+ i)))
+      ((> i n) (reverse accum-list))
+      (if
+        (not (aref sieved-array i))
+        (setf accum-list (cons i accum-list)))
+    )))
+
+(defun sieve-to (n)
+  (let
+    ((multiple-of-primes-p-array (make-array (1+ n))))
+    (do
+      ((i 2 (1+ i)))
+      ((> i n) multiple-of-primes-p-array)
+      (do
+        ((j (* 2 i) (+ i j)))
+        ((> j n) nil)
+        (setf (aref multiple-of-primes-p-array j) t))
+    )))
+
+(time (let ((n 0)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 1)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 2)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 3)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 4)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 5)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 6)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 7)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 100)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 1000)) (format t "primes to ~A: ~A~%" n (length (primes-to n)))))
+(time (let ((n 10000)) (format t "primes to ~A: ~A~%" n (length (primes-to n)))))
+(time (let ((n 100000)) (format t "primes to ~A: ~A~%" n (length (primes-to n)))))
+(time (let ((n 1000000)) (format t "primes to ~A: ~A~%" n (nth (1- 10001) (primes-to n)))))
+
---
+++ 007/main.lisp
@@ -0,0 +1,73 @@
+#! /usr/bin/clisp -C
+
+(defun primes-to (n)
+  ;(reversed-primes-to n))
+  (reverse (reversed-primes-to n)))
+
+(defun reversed-primes-to (n)
+  (cond
+    ((< n 1) nil)
+    ((equal n 1) '(2))
+    ((equal n 2) '(3 2))
+    (t (greater-than-2nd-primes n '(3 2) 2))))
+
+(defun greater-than-2nd-primes (n accum-list num-primes)
+  (if
+    (>= num-primes n)
+    accum-list
+    (do
+      ((x (+ 2 (first accum-list)) (+ 2 x)))
+      ((prime-factor-p x accum-list) (return (greater-than-2nd-primes n (cons x accum-list) (1+ num-primes))))
+      ()
+    )))
+
+(defun prime-factor-p (n reversed-primes)
+  (let
+    ((limit (floor (sqrt n)))
+     (ascending-primes (reverse reversed-primes)))
+    (dolist
+      (p ascending-primes t)
+      (cond
+        ((> p limit) (return t))
+        ((zerop (mod n p)) (return nil))
+        (t nil)))))
+
+(defun prime-p (n)
+  (cond
+    ((< n 2) nil)
+    ((equal n 2) t)
+    ((evenp n) nil)
+    (t
+      (let
+        ((limit (floor (sqrt n))))
+        (do
+          ((i 3 (+ 2 i)))
+          ((> i limit) t)
+          (if
+            (zerop (mod n i))
+            (return nil))
+        )))))
+
+;(let ((n 1)) (format t "~A is prime: ~A~%" n (prime-p n)))
+;(let ((n 2)) (format t "~A is prime: ~A~%" n (prime-p n)))
+;(let ((n 3)) (format t "~A is prime: ~A~%" n (prime-p n)))
+;(let ((n 4)) (format t "~A is prime: ~A~%" n (prime-p n)))
+;(let ((n 5)) (format t "~A is prime: ~A~%" n (prime-p n)))
+;(let ((n 6)) (format t "~A is prime: ~A~%" n (prime-p n)))
+;(let ((n 7)) (format t "~A is prime: ~A~%" n (prime-p n)))
+
+(time (let ((n 0)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 1)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 2)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 3)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 4)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 5)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 6)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 7)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 8)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 9)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 10)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 100)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 1000)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+(time (let ((n 10001)) (format t "primes to ~A: ~A~%" n (primes-to n))))
+
