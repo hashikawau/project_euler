635785b36def1ed758626850b0cf050c401f5a38
hashikawa <hashikawa.yuuichi@gmail.com>
2018-03-27 17:40:59 +0900

    Modified problem 31

--- 031/main.lisp
+++ 031/main.lisp
@@ -2,77 +2,56 @@
 (require 'common)
 (use-package 'common)
 
+;--------------------------------------
+;
+;--------------------------------------
 (defun main ()
-  ;(let
-  ;  ((lst '(1 2 3 4 5 6 7)))
-  ;  (format
-  ;    t
-  ;    "~A: ~A~%"
-  ;    lst
-  ;    (mapcar
-  ;      (p-apply #'count-up lst)
-  ;      (reverse (seq 0 (1- (length lst)))))))
-
-  ;(let
-  ;  ((lst '(0 1 1 2 0 1 1 3)))
-  ;  (format t "~A~%" (to-pound lst)))
-
-  ;(let
-  ;  ((lst '(0 1 1 2 0 1 1)))
-  ;  (format t "~A~%" (to-pound-without-1 lst)))
-
-  ;(let
-  ;  ((lst '(0 1 1 2 0 1 1)))
-  ;  (format
-  ;    t
-  ;    "~A -> ~%~A~%" lst (succ (p-apply #'test-if-under 200) lst)))
 
   (time
-  (let*
-    ((limit 200)
-     (lst-lst (generator limit))
-     ;(lst-sum (mapcar #'to-pound-without-1 lst-lst))
-    )
-    (format t "~A~%" (length lst-lst)) )
+  (format t "~A~%" (length (gen-seq 200)))
   )
-
 )
 
-(defun generator (limit-pound)
+(defun gen-seq (limit &optional (wallet (make-wallet-without-1pence)) (accum nil))
   (let
-    ((init '(0 0 0 0 0 0 0)))
-    (do
-      ((each-coin-numbers-without-1
-         init
-         (succ (p-apply #'test-if-under limit-pound) each-coin-numbers-without-1))
-       (accum
-         nil
-         (cons each-coin-numbers-without-1 accum)))
-      ((null each-coin-numbers-without-1) accum)
-      () )))
+    ((new-wallet (next-wallet limit wallet)))
+    (if (null new-wallet)
+      accum
+      (gen-seq limit new-wallet (cons wallet accum)) )))
 
-(defun succ (test-f each-coin-numbers-without-1)
+;--------------------------------------
+;
+;--------------------------------------
+(defstruct wallet-without-1pence
+  (coin-numbers '(0 0 0 0 0 0 0))
+)
+
+(defun count-coins (wallet)
+  (cond
+    ((wallet-without-1pence-p wallet)
+     (sum (mapcar #'* '(200 100 50 20 10 5 2) (wallet-without-1pence-coin-numbers wallet))) )
+    (t nil) ))
+
+(defun next-wallet (limit wallet)
   (first
     (remove-if-not
-      test-f
-      (count-up-patterns each-coin-numbers-without-1) )))
+      #'(lambda (new-wallet) (<= (count-coins new-wallet) limit))
+      (next-wallet-patterns wallet) )))
 
-(defun test-if-under (limit-pound each-coin-numbers-without-1)
-  (<= (to-pound-without-1 each-coin-numbers-without-1) limit-pound))
-
-(defun to-pound-without-1 (each-coin-numbers-without-1)
-  (to-pound (append each-coin-numbers-without-1 '(0))))
-
-(defun to-pound (each-coin-numbers)
-  (sum
-    (mapcar #'* '(200 100 50 20 10 5 2 1) each-coin-numbers) ))
-
-; (1 2 3 4) -> ((1 2 3 5) (1 2 4 0) (1 3 0 0) (2 0 0 0))
-(defun count-up-patterns (digits)
-  (let
-    ((num-digits (length digits)))
+; #S(wallet (1 2 3 4))
+; -> (
+;     #S(wallet (1 2 3 5))
+;     #S(wallet (1 2 4 0))
+;     #S(wallet (2 3 0 0))
+;     #S(wallet (2 0 0 0))
+;    )
+(defun next-wallet-patterns (wallet)
+  (let*
+    ((digits (wallet-without-1pence-coin-numbers wallet))
+     (num-digits (length digits)))
     (mapcar
-      (p-apply #'count-up digits)
+      #'(lambda (i)
+          (make-wallet-without-1pence :coin-numbers (count-up digits i)))
       (reverse (seq 0 (1- num-digits))) )))
 
 ; (1 2 3 4) 3 -> (1 2 3 5)
@@ -85,7 +64,8 @@
     (list (1+ (nth order digits)))
     (repeat 0 (- (length digits) order 1))))
 
-
-
+;--------------------------------------
+;
+;--------------------------------------
 (main)
 
