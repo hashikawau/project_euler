f1eac3e35b6c168aabace12759e71770a0b22d73
hashikawa <hashikawa.yuuichi@gmail.com>
2018-03-29 09:47:29 +0900

    Modified problem 32

--- 032/main.lisp
+++ 032/main.lisp
@@ -3,50 +3,52 @@
 (use-package 'common)
 
 (defun main ()
-  (time (let ((products (pandigital-products (seq 1 4)))) (format t "--------: sum=~A, ~A~%" (sum (mapcar #'first products)) products)))
-  (time (let ((products (pandigital-products (seq 1 5)))) (format t "--------: sum=~A, ~A~%" (sum (mapcar #'first products)) products)))
-  (time (let ((products (pandigital-products (seq 1 6)))) (format t "--------: sum=~A, ~A~%" (sum (mapcar #'first products)) products)))
-  ;(time (let ((products (pandigital-products (seq 1 8)))) (format t "--------: sum=~A, ~A~%" (sum (mapcar #'first products)) products)))
-  (time (let ((products (pandigital-products (seq 1 9)))) (format t "--------: sum=~A, ~A~%" (sum (mapcar #'first products)) products)))
+  (time (let ((products (enumerate-pandigital-products (seq 1 4)))) (format t "~%sum=~A, ~A~%~%" (sum (mapcar #'first products)) products)))
+  (time (let ((products (enumerate-pandigital-products (seq 1 5)))) (format t "~%sum=~A, ~A~%~%" (sum (mapcar #'first products)) products)))
+  (time (let ((products (enumerate-pandigital-products (seq 1 6)))) (format t "~%sum=~A, ~A~%~%" (sum (mapcar #'first products)) products)))
+  ;(time (let ((products (enumerate-pandigital-products (seq 1 8)))) (format t "~%sum=~A, ~A~%~%" (sum (mapcar #'first products)) products)))
+  (time (let ((products (enumerate-pandigital-products (seq 1 9)))) (format t "~%sum=~A, ~A~%~%" (sum (mapcar #'first products)) products)))
 )
 
-(defun pandigital-products (digit-list)
+(defun enumerate-pandigital-products (digit-lst)
+  (remove-duplicates
+    (reduce
+      #'(lambda (accum permutated-digit-lst)
+          (append
+            accum
+            (find-pandigital-product permutated-digit-lst)))
+      (permutations digit-lst)
+      :initial-value nil)
+    :key #'first)
+)
+
+(defun find-pandigital-product (digit-lst)
   (let
-    ((accum nil))
-    (for-each
-      (permutations digit-list)
-      #'(lambda (lst)
-          (let
-            ((begin 1)
-             (end (1- (length lst))))
-          (do
-            ((i begin (1+ i)))
-            ((>= i end) nil)
-            (do
-              ((j (1+ i) (1+ j)))
-              ((> j end) nil)
-              (let*
-                 (
-                  (a (to-integer (sub-list lst 0 i)))
-                  (b (to-integer (sub-list lst i j)))
-                  (c (to-integer (sub-list lst j)))
-                 )
-                 (if (equal c (* a b))
-                   (progn
-                     (format t "a=~A, b=~A, c=~A, a*b=~A~%" a b c (* a b))
-                     (setf accum (cons (list c a b) accum)))
-                   () ))
-              )))))
-    (remove-duplicates accum :key #'first)
-  )
-)
+    ((begin 1)
+     (end (1- (length digit-lst)))
+     (accum nil))
+    (do
+      ((i begin (1+ i)))
+      ((>= i end) nil)
+      (do
+        ((j (1+ i) (1+ j)))
+        ((> j end) nil)
+        (let
+           ((a (to-integer (sub-list digit-lst 0 i)))
+            (b (to-integer (sub-list digit-lst i j)))
+            (c (to-integer (sub-list digit-lst j))))
+           (if (equal c (* a b))
+             (setf accum (cons (list c a b) accum))
+             () ))))
+    accum))
 
-(defun to-integer (digits)
+
+(defun to-integer (digit-lst)
   (sum
     (mapcar
       #'*
-      (order-lst (1- (length digits)))
-      (reverse digits) )))
+      (order-lst (1- (length digit-lst)))
+      (reverse digit-lst) )))
 
 (defun order-lst (limit)
   (mapcar
